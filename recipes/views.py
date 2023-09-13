b86afae69596709fc2a09ce9bdf44bbf563f6589
Marcus <marcusbarcelos2001@gmail.com>
2023-09-13 08:53:46 -0300

    exemplo de template

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html', {
+        'name': 'Marcus'
+    })
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
