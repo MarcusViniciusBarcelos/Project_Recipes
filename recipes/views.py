a4c1c11fba262fc66d0c05ff4a27954555e7785a
Marcus <marcusbarcelos2001@gmail.com>
2023-09-14 12:19:48 -0300

    criando card recipe

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'Marcus'
     })
