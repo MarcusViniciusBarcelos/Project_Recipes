2fe0cd97a3634ab53a6f51528d0a8f2e48f52f71
Marcus <marcusbarcelos2001@gmail.com>
2023-09-18 16:32:11 -0300

    ajustando home

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
