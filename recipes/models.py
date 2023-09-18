2a4af78bc9d1dca94dd157b471649afa905a1a62
Marcus <marcusbarcelos2001@gmail.com>
2023-09-18 16:10:23 -0300

    ajustando filtros

--- recipes/models.py
+++ recipes/models.py
@@ -22,11 +22,13 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/',
+                              blank=True, default='')
 
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
     def __str__(self):
         return self.title
