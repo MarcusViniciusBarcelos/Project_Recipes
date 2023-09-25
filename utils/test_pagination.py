8e916328917db164bbe3988d1e8b3a28cfaa2d60
Marcus <marcusbarcelos2001@gmail.com>
2023-09-25 20:58:18 -0300

    criando paginação/fazendo estilização

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -7,11 +7,50 @@ class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
-            qtd_pages=4,
+            qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
 
         self.assertEqual([1, 2, 3, 4], pagination)
 
     def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=5,
+        )['pagination']
+        self.assertEqual([4, 5, 6, 7], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )['pagination']
+
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_make_sure_middle_ranges_are_correct(self):
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=5,
+        )['pagination']
+        self.assertEqual([4, 5, 6, 7], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )['pagination']
+
+        self.assertEqual([3, 4, 5, 6], pagination)
+
+    def test_make_pagination_range_is_static_when_last_page_is_next(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20,
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
