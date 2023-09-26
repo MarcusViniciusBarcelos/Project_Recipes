5f4ef63f93f6dd75ea365bb409f977a4cecb8d3b
Marcus <marcusbarcelos2001@gmail.com>
2023-09-25 22:14:13 -0300

    incluindo o dotenv e ajustando uma constante de ambiente

--- recipes/views.py
+++ recipes/views.py
@@ -1,31 +1,21 @@
+import os
+
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
+PER_PAGE = os.environ.get('PER_PAGE', 9)
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
-
-    try:
-        current_page = int(request.GET.get('page', 1))
-
-    except ValueError:
-        current_page = 1
-
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        6,
-        current_page
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -37,9 +27,12 @@ def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id, is_published=True,).order_by('-id'))
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category |'
+        'recipes': page_obj,
+        'title': f'{recipes[0].category.name} - Category |',
+        'pagination_range': pagination_range,
     })
 
 
@@ -64,9 +57,12 @@ def search(request):
         ),
         is_published=True
     ).order_by('-id')
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'search for "{ search_term }" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={ search_term }',
     })
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 
-from utils.pagination import make_pagination_range
+from django.http import HttpRequest
+from django.urls import reverse
+
+from recipes.models import Recipe
+from recipes.tests.test_recipe_base import RecipeTestBase
+from utils.pagination import make_pagination, make_pagination_range
 
 
-class PaginationTest(TestCase):
+class PaginationTest(RecipeTestBase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
@@ -54,3 +59,52 @@ class PaginationTest(TestCase):
             current_page=20,
         )['pagination']
         self.assertEqual([17, 18, 19, 20], pagination)
+
+    def test_pagination_range_start_and_stop(self):
+        page_range = list(range(1, 21))
+        qty_pages = 6
+        current_page = 1  # Simulate a current page close to the start
+        pagination = make_pagination_range(
+            page_range, qty_pages, current_page)['pagination']
+        self.assertEqual([1, 2, 3, 4, 5, 6], pagination)
+
+        current_page = 18  # Simulate a current page close to the end
+        pagination = make_pagination_range(
+            page_range, qty_pages, current_page)['pagination']
+        self.assertEqual([15, 16, 17, 18, 19, 20], pagination)
+
+    def test_invalid_current_page(self):
+        for _ in range(30):
+            self.make_recipe(
+                slug=f'test-pagination-{_}',
+                author_data={
+                    'username': {_}
+                }
+            )
+        request = HttpRequest()
+        request.GET['page'] = 'invalid'  # Simulate an invalid page value
+        queryset = Recipe.objects.all()  # Provide a valid queryset
+        per_page = 10
+        page_obj, pagination_range = make_pagination(
+            request, queryset, per_page)
+        self.assertEqual(page_obj.number, 1)  # Expect the default current page
+
+    def test_pagination_loads_recipes(self):
+        # need a recipe for this test
+        for _ in range(30):
+            self.make_recipe(
+                slug=f'test-pagination-{_}',
+                author_data={
+                    'username': {_}
+                }
+            )
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20,
+        )['pagination']
+
+        response = self.client.get(reverse('recipes:home'))
+        response_context_recipes = response.context['recipes']
+        expected_recipes_count = 9
+        self.assertEqual(len(response_context_recipes), expected_recipes_count)
