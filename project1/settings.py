557e798c69b83f50117943e66a907c5405409781
Marcus <marcusbarcelos2001@gmail.com>
2023-10-09 15:49:30 -0300

    ajustando ALLOWED_HOSTS E CSRF_TRUSTED_ORIGINS

--- project1/settings.py
+++ project1/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['34.173.97.144']
+CSRF_TRUSTED_ORIGINS = ['http://34.173.97.144']
 
 
 # Application definition
