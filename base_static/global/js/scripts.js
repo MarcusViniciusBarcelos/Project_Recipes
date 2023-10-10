d6abbb4540915189aa711d0912b5d394fba0e310
Marcus <marcusbarcelos2001@gmail.com>
2023-10-10 15:55:17 -0300

    criação de menu de navegação e ajuste de logout

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,16 +1,55 @@
-function my_scope() {
-    const forms = document.querySelectorAll('.form-delete');
+(()=>{
+        const forms = document.querySelectorAll('.form-delete');
 
-    for(const form of forms) {
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-    
-            const confirmed = confirm('Are you sure you want to delete ?');
-    
-            if(confirmed) {
-                form.submit();
-            }
-        });
+        for(const form of forms) {
+            form.addEventListener('submit', function(e) {
+                e.preventDefault();
+        
+                const confirmed = confirm('Are you sure you want to delete ?');
+        
+                if(confirmed) {
+                    form.submit();
+                }
+            });
+        }
+})();
+
+(()=>{
+    const buttonCloseMenu = document.querySelector('.button-close-menu');
+    const buttonShowMenu = document.querySelector('.button-show-menu');
+    const menuContainer = document.querySelector('.menu-container');
+
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+        menuContainer.classList.add(menuHiddenClass);
+    };
+
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+        menuContainer.classList.remove(menuHiddenClass);
+    };
+
+    if(buttonCloseMenu) {
+        buttonCloseMenu.removeEventListener('click', closeMenu);
+        buttonCloseMenu.addEventListener('click', closeMenu);
     }
-}
-my_scope()
+    if(buttonShowMenu) {
+        buttonShowMenu.addEventListener('click', showMenu);
+        buttonShowMenu.addEventListener('click', showMenu);
+    }
+})();
+
+(() => {
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+    const formLogout = document.querySelector('.form-logout');
+
+    for (const link of authorsLogoutLinks) {
+        link.addEventListener('click',(e)=>{
+            e.preventDefault();
+            formLogout.submit();
+        })
+    }
+})();
