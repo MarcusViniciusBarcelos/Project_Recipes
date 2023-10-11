44a29e284303896766c887f6472704daabdce20d
Marcus <marcusbarcelos2001@gmail.com>
2023-10-11 11:28:39 -0300

    Criando um Signal para ser emitido quando um usuário for criado (post_save

--- authors/urls.py
+++ authors/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
         views.DashboardRecipe.as_view(),
         name='dashboard_recipe_edit'
     ),
+    path(
+        'profile/<int:id>',
+        views.ProfileView.as_view(),
+        name='profile'
+    ),
 ]
