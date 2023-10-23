a75dcebd0d0cb1d617940806ebd8698e9e74b711
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-22 23:00:01 -0300

    api ap2

--- project1/settings/cors_headers.py
+++ project1/settings/cors_headers.py
@@ -1,4 +1,4 @@
-from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+# from utils.environment import get_env_variable, parse_comma_sep_str_to_list
 
-CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
-    get_env_variable('CORS_ALLOWED_ORIGINS'))
+# CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+#     get_env_variable('CORS_ALLOWED_ORIGINS'))
