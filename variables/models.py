5d59e821bf4e23026622dd701edd9f44e85c80b6
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-25 04:54:20 -0300

    api 2.0

--- variables/models.py
+++ variables/models.py
@@ -11,9 +11,17 @@ class Questions(models.Model):
 
 
 class Rules(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
     questions = models.ManyToManyField(Questions, blank=True, default=None, )
-    result = models.CharField(max_length=50)
+    result = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
+
+
+class RulesQuestions(models.Model):
+    rule = models.ForeignKey(Rules, on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.rule.name + ' - ' + self.question.title
